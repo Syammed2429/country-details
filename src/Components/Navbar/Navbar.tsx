073ed0dedd182b5ef147e24961167825247ab5cf
Dada Khalandar Bellary <kerry.blig12@gmail.com>
2023-06-18 13:12:00 +0530

    completed the navbar

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,10 +1,37 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Flex, HStack, Text, useColorMode } from "@chakra-ui/react";
+import { FC } from "react";
+import {
+    BsMoonFill,
+    BsSunFill,
+} from "react-icons/bs";
 
-export const Navbar = () => {
+export const Navbar: FC = () => {
+    const { colorMode, toggleColorMode } = useColorMode();
+
     return (
         <>
+
             <Box>
-                Navbar
+                <Flex boxShadow="dark-lg" p="5" alignItems="center" w="100%">
+                    <Text flex="1">Where in the world?</Text>
+                    <HStack fontSize="18">
+                        <Box
+                            _hover={{ cursor: "pointer" }}
+                            display="flex" alignItems="center" onClick={toggleColorMode}>
+                            {colorMode === "light" ? (
+                                <>
+                                    <BsMoonFill />
+                                    <Text ml="2">Dark Mode</Text>
+                                </>
+                            ) : (
+                                <>
+                                    <BsSunFill />
+                                    <Text ml="2">Light Mode</Text>
+                                </>
+                            )}
+                        </Box>
+                    </HStack>
+                </Flex>
             </Box>
         </>
     );
