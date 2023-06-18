19b4c3beb8fd9bda53406699e1af8bcacb73edda
Dada Khalandar Bellary <kerry.blig12@gmail.com>
2023-06-18 15:25:46 +0530

    added the default route for the countries component

--- src/Components/Routers/Routers.tsx
+++ src/Components/Routers/Routers.tsx
@@ -1,10 +1,16 @@
 import { Box } from "@chakra-ui/react";
+import { Navbar } from "../Navbar/Navbar";
+import { Route, Routes } from "react-router-dom";
+import { Countries } from "../Countries/Countries";
 
 export const Routers = () => {
     return (
         <>
             <Box>
-                Routers
+                <Navbar />
+                <Routes>
+                    <Route path='/' element={<Countries />} />
+                </Routes>
             </Box>
         </>
     );
